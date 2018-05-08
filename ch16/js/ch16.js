03b37547aaef5a615f17a33cf60508aa71cc62ad
Daniele Parisi <daniele@parisi.xyz>
2018-05-08 12:52:38 +0200

    colors ex.

--- ch16/js/ch16.js
+++ ch16/js/ch16.js
@@ -1,3 +1,5 @@
+// Counter Exercise
+
 const countButtonElement = document.getElementById("countButton");
 const countSpanElement = document.getElementById("clickCount");
 const deactivateButtonElement = document.getElementById("deactivate");
@@ -14,3 +16,32 @@ countButtonElement.addEventListener("click", updateCounter);
 deactivateButtonElement.addEventListener("click", () =>
   countButtonElement.removeEventListener("click", updateCounter)
 );
+
+// Colors Exercise
+
+const ColorElements = document.querySelectorAll("#colors div");
+
+const changeElementsColor = (nodes, color) => {
+  for (n of nodes) {
+    n.style.backgroundColor = color;
+  }
+};
+
+window.addEventListener("keypress", e => {
+  const key = String.fromCharCode(e.charCode);
+  switch (key) {
+    case "r":
+      changeElementsColor(ColorElements, "red");
+      break;
+    case "y":
+      changeElementsColor(ColorElements, "yellow");
+      break;
+    case "g":
+      changeElementsColor(ColorElements, "green");
+      break;
+    case "b":
+      changeElementsColor(ColorElements, "blue");
+      break;
+    default:
+  }
+});
