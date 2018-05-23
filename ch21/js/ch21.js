f9d5c4e82a8cb2d01c6188f91f3b15e4ee84788d
Daniele Parisi <daniele@parisi.xyz>
2018-05-23 15:39:14 +0200

    fetch ex. 2

--- ch21/js/ch21.js
+++ ch21/js/ch21.js
@@ -10,3 +10,18 @@ fetch(
       languageListElement.insertAdjacentHTML("beforeend", `<li>${lang}</li>`);
     });
   });
+
+const paintingsElement = document.getElementById("paintings");
+
+fetch(
+  "https://raw.githubusercontent.com/bpesquet/thejsway/master/resources/paintings.json"
+)
+  .then(response => response.json())
+  .then(paintings => {
+    paintings.forEach(p => {
+      paintingsElement.insertAdjacentHTML(
+        "beforeend",
+        `<tr><th>${p.name}</th><th>${p.year}</th><th>${p.artist}</th></tr>`
+      );
+    });
+  });
