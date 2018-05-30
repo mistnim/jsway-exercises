ad22ba6f6316c45b5f302f28ceabc3fe68ddeb3d
Daniele Parisi <daniele@parisi.xyz>
2018-05-30 12:43:33 +0200

    Countries ex.

--- ch23/js/ch23.js
+++ ch23/js/ch23.js
@@ -12,3 +12,35 @@ formElement.addEventListener("submit", e => {
       document.getElementById("result").textContent = result;
     });
 });
+
+// Visited Countries Exercise
+
+countries = {
+  name: "Daniele",
+  countries: [
+    {
+      name: "Switzerland",
+      year: 2017
+    },
+    {
+      name: "Denmark",
+      year: 2016
+    }
+  ]
+};
+
+fetch("https://thejsway-server.herokuapp.com/api/countries", {
+  method: "POST",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify(countries)
+})
+  .then(response => response.text())
+  .then(result => {
+    console.log(result);
+  })
+  .catch(err => {
+    console.error(err.message);
+  });
