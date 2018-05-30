6073ec45917262c7e08bb6a9d07d5fb2572be545
Daniele Parisi <daniele@parisi.xyz>
2018-05-30 18:28:55 +0200

    modules ex.

---
+++ ch24/circle/circle.js
@@ -0,0 +1,6 @@
+const circumference = r => 2 * r * Math.PI;
+
+const area = r => r ** 2 * Math.PI;
+
+module.exports.circumference = circumference;
+module.exports.area = area;
---
+++ ch24/circle/index.js
@@ -0,0 +1,7 @@
+const circle = require("./circle.js");
+
+console.log(
+  `The circumference of a circle of radius 3 is ${circle.circumference(3)}`
+);
+
+console.log(`The area of a circle of radius 3 is ${circle.area(3)}`);
